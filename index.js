9a141646ae95e022dc5ff991660e5c9c94c4e177
Aiannn <soktoevayan@gmail.com>
2020-08-04 19:19:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+const zag = document.getElementById("header")
+console.log("Here's your header:", zag.innerHTML)
 
 
 /***** Deliverable 2 *****/
 
+zag.style.color = 'red'
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
+const div = document.querySelector(".player-container")
 
-
+for (let i=0; i<PLAYERS.length; i++) {
+div.innerHTML += `
+    <div class="player" data-number="${PLAYERS[i].number}">
+    <h3>
+    ${PLAYERS[i].name} (<em>${PLAYERS[i].nickname}</em>)
+    </h3>
+    <img src="${PLAYERS[i].photo}" alt="playerphoto">
+    </div>
+`
+}
 /***** Deliverable 4 *****/
+const sterling = document.querySelector("[data-number='7']")
+
+sterling.remove()
